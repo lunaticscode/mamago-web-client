63e53bf165304babf8dc795e5a9e7a47c172ae3a
lunaticscode <lunatics384@gmail.com>
2026-03-02 20:02:50 +0900

    remove: 테스트 파일(컴포넌트, worker) 삭제

--- widgets/containers/home-page/HomeContainer.tsx
+++ widgets/containers/home-page/HomeContainer.tsx
@@ -1,14 +1,10 @@
 "use client";
 import ChatEnterButtons from "@/features/home/ui/ChatEnterButtons";
-import ConvertAudioWorkerLoad from "@/features/home/ui/ConvertAudioWorkerLoad";
-import TestWorkerLoad from "@/features/home/ui/TestWorkerLoad";
 import Container from "@/widgets/layout/Container";
 
 const HomeContainer = () => {
   return (
     <Container>
-      <TestWorkerLoad />
-      <ConvertAudioWorkerLoad />
       <ChatEnterButtons />
     </Container>
   );
